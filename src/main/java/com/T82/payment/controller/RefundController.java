447d92bbcaffd99597befc9f4c12e9b558dd8b39
HuiSeokJeong <tobysug123@gmail.com>
2024-07-19 14:55:01 +0900

    [refactor] 엔드포인트 수정

--- src/main/java/com/T82/payment/controller/RefundController.java
+++ src/main/java/com/T82/payment/controller/RefundController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/payment")
+@RequestMapping("api/v1/refund")
 @RequiredArgsConstructor
 public class RefundController {
     private final RefundService refundService;
